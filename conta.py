7b8eec37f2e73cf0478517bbd3d53c8cde1bcb5d
andrematheus2 <am_arao@hotmail.com>
2022-11-14 11:11:01 -0300

    getters e setters

--- conta.py
+++ conta.py
@@ -1,6 +1,6 @@
 
-class Conta():
-    def __int__(self, numero, titular, saldo, limite):
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
@@ -17,3 +17,15 @@ class Conta():
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    def get_saldo(self):
+            return self.__saldo
+
+    def get_titular(self):
+            return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, limite):
+        self.__limite = limite
