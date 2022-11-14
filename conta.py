12733cfe0ab9a1b80187630d118082bd5536f998
andrematheus2 <am_arao@hotmail.com>
2022-11-14 09:15:15 -0300

    construtor

--- conta.py
+++ conta.py
@@ -1,3 +1,7 @@
 
 class Conta:
-    pass
+    def __int__(self, numero, titular, saldo, limite):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
